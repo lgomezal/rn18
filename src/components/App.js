87c4c8f4ae7dc2ccc7025428c1213e5c1ca5703b
LuisGomezAlonso <lgomezal@gmail.com>
2018-09-21 19:08:32 +0200

    final commit day 2

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { Router, Scene, Actions, Stack } from 'react-native-router-flux'
 import { Houses, Characters } from './sections/'
+import { configureAxios } from '../api'
 
 export default class App extends Component {
 
+    componentWillMount() {
+        configureAxios()
+    }
 
     render() {
         return (
             <Router>
                 <Stack key='root'>
-                    <Scene key='houses' component={Houses} title='Houses' initial={true} />
+                    <Scene key='houses' component={Houses} title='Casas' initial={true} />
                     <Scene key='characters' component={Characters} />
                 </Stack>
             </Router>
--- src/components/sections/houses/view.js
+++ src/components/sections/houses/view.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList, Alert } from 'react-native'
 import { Actions } from 'react-native-router-flux'
+import * as api from '../../../api/'
 import styles from './styles'
 
 export default class Houses extends Component {
 
-    gotoCharacters() {
-        Actions.characters({ title: 'Personajes' })
+    constructor(props) {
+        super(props)
+        this.state = {
+            housesList: [],
+        }
+    }
+
+    componentWillMount() {
+        this._fetchHousesList()
+    }
+
+    _fetchHousesList() {
+        api.fetchHouses().then(response => {
+            //console.log('fetchHouses response: ', response)
+            if (response && response.data && response.data.records) {
+                this.setState({ housesList: response.data.records })
+            }
+        }).catch(error => {
+            console.log('fetchHouses error: ', error)
+            this.setState({ housesList: [] })
+        })
+    }
+
+    _onHouseTapped(house) {
+        Alert.alert('Casa:', house.nombre)
+    }
+
+    _renderItem({ item }) {
+        return <HouseCell house={item} onHousePress={(v) => this._onHouseTapped(v)} />
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={{ color: 'white', fontWeight: 'bold' }}>CASAS</Text>
-
-                <Button
-                    title={'Pulsar para ir a Characters'}
-                    color={'lime'}
-                    onPress={() => this.gotoCharacters()}
+                <FlatList
+                    data={this.state.housesList}
+                    renderItem={value => this._renderItem(value)}
+                    keyExtractor={(item, i) => 'cell' + item.id}
                 />
             </View>
+        )
+    }
+}
+
+class HouseCell extends Component {
+
+    static defaultProps = {
+        house: null,
+        onHousePress: () => { },
+    }
+
+    render() {
+        const { house } = this.props
+        const name = house ? house.nombre : ''
+        return (
+            <TouchableOpacity onPress={() => this.props.onHousePress(house)} style={{ height: 120, borderWidth: 1, borderColor: 'blue', alignItems: 'center', justifyContent: 'center' }}>
+                <Text>{name}</Text>
+            </TouchableOpacity>
         )
     }
 }
